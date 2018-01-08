8f8c77559b30b25ceca0af100ce259bd979030fc
Jon Kelly <jon_k@live.ca>
2018-01-08 13:09:50 -0500

    Paddle Moves to edge of screen
    Also expanded the touch input colliders

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Paddle : MonoBehaviour
 {
 	public float paddleSpeed = 1f;
 
-	public float leftRange = 1.6f;
-	public float rightRange = 18.9f;
+	private float _leftRange;
+	private float _rightRange;
 
 	private float _yPos;
 	private float _direction;
@@ -26,6 +27,11 @@ public class Paddle : MonoBehaviour
 		GameManager.OnLeftTouch += MoveLeft;
 		GameManager.OnRightTouch += MoveRight;
 		GameManager.OnTouchRelease += TouchRelease;
+
+		var renderer = GetComponent<SpriteRenderer> ();
+
+		_leftRange = GameManager.Instance.LeftBorder + (renderer.bounds.size.x / 2);
+		_rightRange = GameManager.Instance.RightBorder - (renderer.bounds.size.x/2);
 	}
 
 	void MoveLeft () 
@@ -46,7 +52,7 @@ public class Paddle : MonoBehaviour
 	void FixedUpdate()
 	{
 		float xPos = transform.position.x + (_direction * paddleSpeed);
-		var playerPos = new Vector3 (Mathf.Clamp (xPos, leftRange, rightRange), _yPos, 0f);
+		var playerPos = new Vector3 (Mathf.Clamp (xPos, _leftRange, _rightRange), _yPos, 0f);
 		transform.position = playerPos;
 	}
 }
